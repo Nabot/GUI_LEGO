c83bc597c84fcdd1d7b60273eabe44a03f84264b
Nabot <nabot@Nabot.local>
2017-05-04 20:28:01 +0200

    Created login % Logout URL

--- GUI_LEGO/urls.py
+++ GUI_LEGO/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+# Login/Logout URLs
+(r'^login/$',
+    'django.contrib.auth.views.login', {'template_name': 'login.html'}
+),
+(r'^logout/$',
+    'django.contrib.auth.views.logout', {'next_page': '/login/'}
+),
 ]
+
