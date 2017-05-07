fdf2d55f908ab62a2fe7bf26b2b54f30857afc05
phungmai <ictphungmai@gmail.com>
2017-05-07 18:28:53 +0700

    add views.py

---
+++ GUI_LEGO/views.py
@@ -0,0 +1,16 @@
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'login.html', c)
+
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username = username, password = password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/accounts/loggedin')
+    else:
+        return HttpResponseRedirect('/accounts/invalid')
